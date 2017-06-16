08323f54db6f042faeb518a60427b516f44311ad
Dhivya Ramalingam <dhivya.ramalingam.ap@nielsen.com>
2017-06-16 19:10:39 -0400

    First Commit

---
+++ TestWCFApplication1/Service2.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TestWCFApplication1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service2" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service2.svc or Service2.svc.cs at the Solution Explorer and start debugging.
+    [ServiceContract]
+    public class Service2
+    {
+        [OperationContract]
+        public int DoWork()
+        {
+            return new Random().Next(0, 100);
+        }
+    }
+}
---
+++ TestWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Service2Reference.Service2Client svc2Obj = new Service2Reference.Service2Client();
+            int value =  svc2Obj.DoWork();
+            ViewData["val"] = value;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
